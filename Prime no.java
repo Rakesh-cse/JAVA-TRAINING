a0537d1a653446d95abeccf28aa11ad0b3eeaa0d
Rakesh-cse <rakeshoff2006@gmail.com>
2026-08-22 12:57:12 +0530

    Add files via upload

---
+++ Prime no.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        for (int i = 250; i <= 750; i++) {
+            int temp = i;
+            int count = 0;
+
+            while (temp > 0) {
+                int digit = temp % 10;
+                
+                if (digit == 2 || digit == 3 || digit == 5 || digit == 7) {
+                    count++;
+                }
+                
+                temp = temp / 10;
+            }
+
+            if (count == 2) {
+                System.out.println(i);
+            }
+        }
+        
+    }
+}
