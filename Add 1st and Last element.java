6ce1d3d60942c8d61c6370da78b969d29d2b223f
Rakesh-cse <rakeshoff2006@gmail.com>
2026-08-22 12:56:05 +0530

    Add files via upload

---
+++ Add 1st and Last element.java
@@ -0,0 +1,13 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int[]a={2,3,4,5,6};
+	    for(int i=0;i<a.length;i++){
+	        for(int j=a.length-1;i<a.length;j--){
+	            System.out.println(a[i]+a[j]);
+	           
+	        }
+	    }
+
+	}
+}
