bde37c0c03178812c236e44a3f63fc78a0a63e41
Rakesh-cse <rakeshoff2006@gmail.com>
2026-08-22 12:59:59 +0530

    Add files via upload

---
+++ Upper and Lower Case.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String a="Praveenn";
+        String b=a.toUpperCase();
+        String c=a.toLowerCase();
+        System.out.println(b);
+        System.out.println(c);
+        
+        
+    }
+}
